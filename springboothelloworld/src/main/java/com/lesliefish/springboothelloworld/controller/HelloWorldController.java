212d1e7d1976c51e2ea8eb3add5c6f2578d4fc4f
leiyu <yuleics@outlook.com>
2019-07-04 18:18:21 +0800

    hello world and test passed

--- springboothelloworld/src/main/java/com/lesliefish/springboothelloworld/controller/HelloWorldController.java
+++ springboothelloworld/src/main/java/com/lesliefish/springboothelloworld/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.lesliefish.springboothelloworld.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RestController 的意思就是 Controller 里面的方法都以 json 格式输出
 @RestController
 public class HelloWorldController {
     @RequestMapping("/hello")
