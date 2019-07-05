188f4591fa2b353391a6a62129bdf729bdd09094
leiyu <yuleics@outlook.com>
2019-07-05 16:06:36 +0800

    fixed bug

--- web/src/main/java/com/lesliefish/web/controller/SimpleController.java
+++ web/src/main/java/com/lesliefish/web/controller/SimpleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SimpleController {
 
-    @Value("$spring.application.name")
+    @Value("${spring.application.name}")
     String appName;
 
     @RequestMapping("/")
