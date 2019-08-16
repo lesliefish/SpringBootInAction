bf8cd36e2a4c31c1d59799fac2b98fa2fcad60ef
leiyu <yuleics@outlook.com>
2019-08-16 16:19:33 +0800

    spring boot hello world unit test

---
+++ helloworld/src/test/java/com/lesliefish/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,37 @@
+package com.lesliefish.helloworld;
+
+import com.lesliefish.helloworld.controller.HelloWorldController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorldApplicationTests {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+    }
+
+    @Test
+    public void getInfo() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Spring Boot Hello World!")));
+    }
+
+}
