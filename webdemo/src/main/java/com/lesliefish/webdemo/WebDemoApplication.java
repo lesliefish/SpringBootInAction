a1cc5a9253bc65d59fb2b3900d3041c8bcbc9bb4
leiyu <yuleics@outlook.com>
2019-08-22 21:01:41 +0800

    hello controller test ok

--- webdemo/src/main/java/com/lesliefish/webdemo/WebDemoApplication.java
+++ webdemo/src/main/java/com/lesliefish/webdemo/WebDemoApplication.java
@@ -2,12 +2,13 @@ package com.lesliefish.webdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class WebDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebDemoApplication.class, args);
+    }
 
 }
