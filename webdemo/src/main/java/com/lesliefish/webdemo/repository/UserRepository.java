cbf855c4a59cc5151f7adb97ec8d1dc75603c3c9
leiyu <yuleics@outlook.com>
2019-08-23 16:38:57 +0800

    add user controller

--- webdemo/src/main/java/com/lesliefish/webdemo/repository/UserRepository.java
+++ webdemo/src/main/java/com/lesliefish/webdemo/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.lesliefish.webdemo.repository;
 import com.lesliefish.webdemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByUserName(String userName);
+    public User findByUserName(String userName);
 
-    User findByUserNameOrEmail(String userName, String email);
+    public User findByUserNameOrEmail(String userName, String email);
+
+    public List<User> findAll();
 }
